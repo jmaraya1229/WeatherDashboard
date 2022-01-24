69ef4409e8cad3428f37c818d65f8778e8215662
jmaraya1229 <jessemaraya2013@gmail.com>
2022-01-24 00:37:08 -0800

    created forecast day 1

--- Assets/JS/app.js
+++ Assets/JS/app.js
@@ -2,11 +2,11 @@ var cityInput = document.getElementById("city-text");
 var cityForm = document.getElementById("city-form")
 var cityHistory = document.getElementById("city-history");
 const myKey = "1caeb144b3d52228ebc13b0c9b582ab2";
-var locationData = "";
 var currentCityContainer = document.getElementById("currentContainer");
 var cities = [];
 
 function renderHistory() {
+    cityHistory.innerHTML = "";
     for (var i = 0; i < cities.length; i++) {
         var city = cities[i]
         var li = document.createElement("li");
@@ -15,7 +15,6 @@ function renderHistory() {
         li.classList.add("box", "button", "has-text-left");
         cityHistory.appendChild(li);
   }
-
 }
 
 function init() {
@@ -33,12 +32,15 @@ function storeCities() {
  
 var submitButton = document.getElementById("submit");
 submitButton.addEventListener("click", function(event) {
-    
+    event.preventDefault();
     if (cityInput.value === "") {
         return;
     }
 
-    locationData = getCurrent(cityInput.value);
+    getCurrent(cityInput.value)
+    .then(result => {
+        document.getElementById("currentCityName").innerHTML = result.name;
+    })
 
     getCurrent(cityInput.value)
     .then(result => getOneCall(result.coord.lat, result.coord.lon))
@@ -46,12 +48,23 @@ submitButton.addEventListener("click", function(event) {
         populateCurrentCity(finalResult);
     })
 
+    getFiveDayForeCast(cityInput.value)
+    .then(result => {
+        // Day 1
+        let icon = result.list[0].weather[0].icon;
+        console.log(icon)
+        document.getElementById("day-one-date").innerHTML = convertUnixToDate(result.list[0].dt);
+        document.getElementById("day-one-icon").src = "http://openweathermap.org/img/w/" + icon + ".png";
+        document.getElementById("day-one-temp").innerHTML = "Temp: " + result.list[0].main.temp + "&deg;F"; 
+        document.getElementById("day-one-wind").innerHTML = "Wind: " + result.list[0].wind.speed + " mph";
+        document.getElementById("day-one-humidity").innerHTML = "Humidity: " + result.list[0].main.humidity + "%";
+    })
+
     cities.push(cityInput.value);
     cityInput.value = "";
 
     storeCities();
     renderHistory();
-    event.preventDefault();
 });
 
 init();
@@ -82,14 +95,27 @@ async function getOneCall(lat, lng) {
       return data;
 }
 
-function populateCurrentCity(currentCityValues) {
-    console.log(currentCityValues)
-    const unixTimestamp = currentCityValues.current.dt;
+async function getFiveDayForeCast(city_name) {
+    const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${city_name}&units=imperial&appid=${myKey}`
+        );
+  
+      const data = await response.json();
+  
+      return data;
+}
+
+function convertUnixToDate(unix_time){
+    const unixTimestamp = unix_time;
     const milliseconds = unixTimestamp * 1000;
     const dateObject = new Date(milliseconds);
     let options = {month:'numeric', day:'numeric', year:'numeric'};
-    const humanDateFormat = dateObject.toLocaleString('en-US', options);
+    
+    return dateObject.toLocaleString('en-US', options);
+}
 
+function populateCurrentCity(currentCityValues) {
+    const humanDateFormat = convertUnixToDate(currentCityValues.current.dt)
     let UVColor = currentCityValues.current.uvi
 
     function changeUVColor() {
@@ -104,10 +130,36 @@ function populateCurrentCity(currentCityValues) {
         return UVColor;
     }
 
-    document.getElementById("currentCityName").innerHTML = "San Diego";
     document.getElementById("currentDate").innerHTML = humanDateFormat;
-    document.getElementById("currentTemp").innerHTML = "Temp:" + currentCityValues.current.temp + "&deg; F";
-    document.getElementById("currentWindSp").innerHTML = "Wind:" + currentCityValues.current.wind_speed + " mph";
+    document.getElementById("currentTemp").innerHTML = "Temp: " + currentCityValues.current.temp + "&deg;F";
+    document.getElementById("currentWindSp").innerHTML = "Wind: " + currentCityValues.current.wind_speed + " mph";
     document.getElementById("currentHumidity").innerHTML = "Humidity: " + currentCityValues.current.humidity + "%";
     document.getElementById("currentUVI").innerHTML = "UV Index: " + changeUVColor();
 }
+
+cityHistory.addEventListener("click", function() {
+    for (var i = 0; i < cityHistory.childNodes.length; i++){
+        var savedCity = cityHistory.childNodes[i]
+        savedCity.addEventListener("click", function(event) {
+            console.log(event.target.innerHTML)
+            if (event.target.innerHTML === null) {
+                return;
+            }
+            else{
+                getCurrent(event.target.innerHTML)
+                .then(result => {
+                    document.getElementById("currentCityName").innerHTML = result.name;
+                })
+        
+                getCurrent(event.target.innerHTML)
+                .then(result => getOneCall(result.coord.lat, result.coord.lon))
+                .then(finalResult => {
+                    populateCurrentCity(finalResult);
+                })
+                cityHistory.innerHTML = "";
+                renderHistory()
+            }
+        });
+    }
+});
+
